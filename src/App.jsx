dee13ba32ec2b0bcb20dde5489b836efff0478dd
Audrey Jo <audreyjo35@gmail.com>
2022-03-10 18:44:24 +0000

    react classes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
-const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
-const helloContinents = Array.from(continents, (c) => `Hello ${c}!`);
-const message = helloContinents.join(" ");
+class HelloWorld extends React.Component {
+  render() {
+    const continents = ["Africa", "America", "Asia", "Australia", "Europe"];
+    const helloContinents = Array.from(continents, (c) => `Hello ${c}!`);
+    const message = helloContinents.join(" ");
 
-// use JSX to construct an element or an element hierarchy
-// rather than nesting React.createElement() calls
-const element = (
-  <div title="Outer div">
-    <h1>{message}</h1>
-  </div>
-);
+    // use JSX to construct an element or an element hierarchy
+    // rather than nesting React.createElement() calls
+    return (
+      <div title="Outer div">
+        <h1>{message}</h1>
+      </div>
+    );
+  }
+}
+
+const element = <HelloWorld />;
 
 // render the React elements and actually display them on the site
 ReactDOM.render(element, document.getElementById("content"));
